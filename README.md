64eaefbc7a8bce65219cbdbe4094e707a9682ad8
tcm_NIMBUS <tomconmo@gmail.com>
2021-03-17 16:34:04 -0400

    Initial commit from gatsby: (https://github.com/ehowey/gatsby-starter-catalyst-hydrogen.git)

---
+++ README.md
@@ -0,0 +1,24 @@
+# Gatsby Starter Catalyst Hydrogen
+
+This is a full-featured starter that ties together the core theme, header and footer themes and the hydrogen theme to ceate a complete website. The theme relies on data from [SANITY.io](https://www.sanity.io/) and includes a customized content studio for the CMS.
+
+## Documentation
+
+[https://www.gatsbycatalyst.com/](https://www.gatsbycatalyst.com)
+
+## Catalyzing Start
+
+```sh
+# create a new Gatsby site using the catalyst writer starter site
+gatsby new catalyst-hydrogen https://github.com/ehowey/gatsby-starter-catalyst-hydrogen
+```
+
+[Read the Gatsby Quick Start Guide](https://www.gatsbyjs.com/docs/quick-start)
+
+## Demo
+
+[https://gatsby-starter-catalyst-hydrogen.netlify.app/](https://gatsby-starter-catalyst-hydrogen.netlify.app/)
+
+## About Gatsby Theme Catalyst
+
+The Catalyst series of themes and starters for [GatsbyJS](https://www.gatsbyjs.com/) were designed to provide an opinionated set of integrated themes and starters that can be used to accelerate your next Gatsby project. The vision is for one "core" theme in which most dependencies and components are contained followed by progressively more styled and refined child themes and starters. These themes rely heavily on [Theme-UI](https://theme-ui.com/) and [MDX](https://mdxjs.com/getting-started/gatsby/).
---
+++ sanity-studio/structure/deskStructure.js
@@ -0,0 +1,103 @@
+import S from "@sanity/desk-tool/structure-builder"
+import {
+  MdSettings,
+  MdShare,
+  MdList,
+  MdInfo,
+  MdHome,
+  MdInsertDriveFile,
+  MdPerson,
+  MdDashboard,
+} from "react-icons/md"
+
+const hiddenDocTypes = (listItem) =>
+  ![
+    "siteSettings",
+    "menuLink",
+    "socialLink",
+    "project",
+    "post",
+    "homePage",
+    "work",
+    "categories",
+    "logos",
+    "page",
+    "theme",
+    "workPage",
+    "author",
+    "category",
+  ].includes(listItem.getId())
+
+export default () =>
+  S.list()
+    .title("Content")
+    .items([
+      S.listItem()
+        .title("Site Settings")
+        .icon(MdSettings)
+        .child(
+          S.list()
+            .title("Site Settings")
+            .items([
+              S.listItem()
+                .title("Metadata")
+                .icon(MdInfo)
+                .child(
+                  S.document()
+                    .title("Metadata")
+                    .schemaType("siteSettings")
+                    .documentId("siteSettings")
+                ),
+              S.listItem()
+                .title("Authors")
+                .icon(MdPerson)
+                .child(S.documentTypeList("author").title("Author")),
+              S.listItem()
+                .title("Navigation Links")
+                .icon(MdList)
+                .child(
+                  S.documentTypeList("menuLink").title("Navigation Links")
+                ),
+
+              S.listItem()
+                .title("Social Links")
+                .icon(MdShare)
+                .child(S.documentTypeList("socialLink").title("Social Links")),
+            ])
+        ),
+      // Add a visual divider (optional)
+      S.divider(),
+      S.listItem()
+        .title("Home Page")
+        .icon(MdHome)
+        .child(S.editor().schemaType("homePage").documentId("homePage")),
+      S.listItem()
+        .title("Work Page")
+        .icon(MdInsertDriveFile)
+        .child(S.editor().schemaType("workPage").documentId("workPage")),
+      S.listItem()
+        .title("Work")
+        .child(
+          S.list()
+            .title("Work")
+            .items([
+              S.listItem()
+                .title("List of Work")
+                .schemaType("work")
+                .child(S.documentTypeList("work").title("List of Work")),
+              S.listItem()
+                .title("Categories")
+                .schemaType("categories")
+                .child(S.documentTypeList("categories").title("Categories")),
+              S.listItem()
+                .title("Featured Logos")
+                .schemaType("logos")
+                .child(S.documentTypeList("logos").title("Publisher Logos")),
+            ])
+        ),
+      S.listItem()
+        .title("Other Pages")
+        .child(S.documentTypeList("page").title("Pages")),
+
+      ...S.documentTypeListItems().filter(hiddenDocTypes),
+    ])
