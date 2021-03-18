ab7f170f3e6e86a9d381001a2e2a61a64b1cc630
tcm_NIMBUS <tomconmo@gmail.com>
2021-03-17 23:56:46 -0400

    oops, don't duplicate the url in the dashboard config

--- sanity-studio/src/dashboardConfig.js
+++ sanity-studio/src/dashboardConfig.js
@@ -8,13 +8,13 @@ export default {
           {
             title: 'Sanity Studio',
             apiId: 'e382d589-9a7c-4bdd-8908-22cda7b432b7',
-            buildHookId: 'https://api.netlify.com/build_hooks/60528f7785a111183dd985f2',
+            buildHookId: '60528f7785a111183dd985f2',
             name: 'pscsanity',
           },
           {
             title: 'Website',
             apiId: 'bebb71dc-26c4-44e8-9ab6-4077955d99c3',
-            buildHookId: 'https://api.netlify.com/build_hooks/60528f5b63ca2c165d5fc800',
+            buildHookId: '60528f5b63ca2c165d5fc800',
             name: 'peopleschool'
           }
         ]
